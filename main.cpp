99ef022877e1dcc709200e2dfd2138c36032aae3
AndrewCPU2 <ariley21@stac.edu>
2024-10-06 18:14:10 -0400

    add main C++ program

---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include "GameEngine.h"
+
+using namespace std;
+
+int main() {
+    GameEngine* engine = GameEngine::getInstance();
+
+    // Register the players
+    engine->registerPlayer("Player 1");
+    engine->registerPlayer("Player 2");
+
+    // Trigger game events
+    engine->triggerEvent("Enemy appeared!");
+    engine->triggerEvent("Treasure found!");
+
+    // Simulate player actions
+    engine->playerAction("Player1", "attacks the enemy.");
+    engine->playerAction("Player2", "collects the treasure.");
+
+    // Another game event and player action
+    engine->triggerEvent("Player leveled up!");
+    engine->playerAction("Player1", "casts a spell.");
+
+    return 0;
+}
+
+/**
+ * Expected Output:
+ *
+ * [GameEngine] Event Triggered: Enemy appeared!
+ * [GameEngine] Event Triggered: Treasure found!
+ * [GameEngine] Player1 attacks the enemy.
+ * [GameEngine] Player2 collects the treasure.
+ * [GameEngine] Event Triggered: Player leveled up!
+ * [GameEngine] Player1 casts a spell.
+ */
